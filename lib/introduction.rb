3d6b14036c1586b0c996bf9ebfe7321f245fa02e
Learn.co IDE <foo@example.com>
2016-12-20 14:58:31 +0000

    Done.

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -1,8 +1,7 @@
 def introduction(str_value)
-  print "Hi, my name is #{str_value}.\n"  
+  print "Hi, my name is #{str_value}.\n"
 end
 
 def introduction_with_language(str_name, str_language)
   print "Hi, my name is #{str_name} and I am learning to program in #{str_language}.\n"
 end
-  
